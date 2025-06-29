3ef38bb1bb104f31af04fe4abfc91d0e75a06ebb
Fa_Skyz <71993367+luthfaismusyaffa@users.noreply.github.com>
2025-06-29 22:56:59 +0700

    first upload

---
+++ src/Data/Dummy.js
@@ -0,0 +1,26 @@
+// Dummy user untuk login
+export const dummyUser = {
+  id: 1,
+  email: "admin@gmail.com",
+  password: "123456",
+  name: "Admin",
+};
+
+// Dummy data mahasiswa
+export const mahasiswaList = [
+  {
+    nim: "111202214508",
+    nama: "imbi",
+    prodi: "teknik mesin",
+  },
+  {
+    nim: "111202214500",
+    nama: "Nolan",
+    prodi: "kedokteran",
+  },
+  {
+    nim: "111202214521",
+    nama: "Rehan",
+    prodi: "TI",
+  },
+];
